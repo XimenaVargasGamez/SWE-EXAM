fd6972584b9e9536e1568a059ea99f211d980195
Ximena Vargas <ximenavargas.work@gmail.com>
2025-08-14 01:54:12 -0700

    Update Main-Page

--- my-next-app/src/app/page.tsx
+++ my-next-app/src/app/page.tsx
@@ -2,16 +2,21 @@ import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
 import Image from "next/image";
 import Link from "next/link";
-import { Anton } from 'next/font/google'
+import { Be_Vietnam_Pro, Anton } from 'next/font/google';
+
+const beVietnamPro = Be_Vietnam_Pro({
+  weight: ['400', '500', '700'],
+  subsets: ['latin'],
+});
 
 const anton = Anton({
   weight: '400',
   subsets: ['latin'],
-})
+});
 
 export default function MainPage() {
   return (
-    <div className="min-h-[1000px] flex flex-col">
+    <div className="min-h-[1150px] flex flex-col"> 
       <NavBar />
 
       <div className="flex-1 relative">
@@ -23,18 +28,20 @@ export default function MainPage() {
           priority
         />
 
-        <main className="relative z-10 flex items-center justify-center min-h-[700px] px-[24px]">
+        <main className="relative z-10 flex items-center justify-center min-h-[900px] px-[24px]">
           <div className="text-center text-[#FFFEFB]">
             <h1 className="text-[70px] font-['Anton'] tracking-[4px] mb-[-30px]">
               Examen LKMX
             </h1>
 
-            <p className="text-[20px] font-['Anton'] tracking-[8px] mb-[100px] md:text-[50px] uppercase ">
+            <p className="text-[20px] font-['Anton'] tracking-[4px] mb-[100px] md:text-[50px] uppercase ">
               XIMENA VARGAS GÁMEZ
             </p>
 
             <Link href="/usuarios">
-              <button className="bg-white text-blue-600 px-[48px] py-[16px] rounded-lg text-[18px] font-semibold transition-colors duration-200 shadow-lg">
+              <button
+                className={`${beVietnamPro.className} text-[20px] text-[#0071BD] bg-[#FDFBFF] border-none px-[105px] py-[15px] rounded-[13px] shadow-[500px] transition-transform duration-200 hover:scale-105`}
+              >
                 Iniciar
               </button>
             </Link>
